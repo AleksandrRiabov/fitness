cca458b8d29a4aba3c14dbfc33b74f9f900f2598
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-02-11 23:26:43 +0000

    Initial commit, starter page added

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+
+import Exercises from '../components/Exercises';
+import SearchExcercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExcercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
